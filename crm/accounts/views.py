48eb432e4b48b23bac5decbde45b6c226d8003a9
Quinn Le <quynhnle135@gmail.com>
2022-12-14 18:18:15 -0500

    Added products view and customer views then wired them into urls file

--- crm/accounts/views.py
+++ crm/accounts/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+
+def home(request):
+    return HttpResponse('home')
+
+def products(request):
+    return HttpResponse('products')
+
+def customer(request):
+    return HttpResponse('customer')
 
 # Create your views here.
